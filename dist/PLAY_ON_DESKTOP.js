d59ae31616b0b7442706d846b63327e7815b1705
zamasu <zxm273293@gmail.com>
2025-12-10 14:17:47 +0300

    Add PLAY_ON_DESKTOP quest configuration

---
+++ dist/PLAY_ON_DESKTOP.js
@@ -0,0 +1,30 @@
+const { QuestConfig } = require('../lib/questConfig.js');
+const ms = require('ms');
+module.exports = new QuestConfig({
+    name: "PLAY_ON_DESKTOP",
+    async run(user) {
+        const secondsNeeded = user.target;
+        let progress = user.current || 0;
+        while (!user.stoped) {
+            const heartbeat = await user.api
+                .post(`/quests/${user.quest}/heartbeat`, {
+                stream_key: `call:${user.quest}:1`,
+                terminal: false
+            }).catch((err) => err.response);
+            if (!heartbeat?.data?.user_id) {
+                user.stop("Error sending heartbeat");
+                break;
+            }
+            const response = user.extractProgress(heartbeat.data);
+            progress = response.value;
+            user.sendUpdate(progress, response.completed === true);
+            if (progress >= secondsNeeded ||
+                response?.completed === true) {
+                user.stop();
+                user.completed = true;
+                break;
+            }
+            await user.delay(ms("30s"));
+        }
+    },
+});
